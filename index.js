6448283e95b4a5aef8ddf5d83dca3b3e805a70b9
Andy Chow <andychchow326@gmail.com>
2022-09-15 00:43:47 +0800

    Add background

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-let monthEl = document.getElementById("month-el");
-let timeEl = document.getElementById("time-el");
+const cardElement = document.getElementById("card-wrapper");
+const card = cardElement.getBoundingClientRect();
+const cardWidth = card.width;
 
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-const m = new Date();
-let month = months[m.getMonth()];
-
-var d = new Date();
-var dd = String(d.getDate()).padStart(2, "0");
-
-let min = "0";
-if (d.getMinutes() > 0 && d.getMinutes() <= 9) {
-  min = "0" + d.getMinutes();
-} else if (d.getMinutes() === "0") {
-  min = "00";
-} else min = d.getMinutes();
-
-var time = d.getHours() + "" + min;
-
-monthEl.textContent = month + " " + dd;
-timeEl.textContent = time;
-
-let changingDateEl = document.getElementById("changingDate");
+"load resize maximize minimize orientationchange"
+  .split(" ")
+  .forEach((event) => {
+    window.addEventListener(event, () => {
+      const sizerElement = document.getElementById("card-sizer");
+      const sizer = sizerElement.getBoundingClientRect();
+      const newWidth = sizer.width;
+      cardElement.style.transform = `scale( ${newWidth / cardWidth} )`;
+    });
+  });
